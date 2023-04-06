89128ce99b998228d55db7d258ce203221f2de9b
Franco Beber <fbeber73@gmail.com>
2023-04-06 19:27:09 -0300

    MOdifico para reentrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,94 +1,37 @@
 import fs from "fs";
-import { title } from "process";
 
 class ProductManager {
   #products;
   #path;
 
   constructor(path) {
+    this.#path = path;
     this.#products = [];
-    this.path = path;
+    this.loadProducts();
   }
 
-  #generateId = () => {
-    let id;
-    if (this.#products.length === 0) {
-      id = 1;
-    } else {
-      id = this.#products[this.#products.length - 1].id + 1;
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.#path, "utf-8");
+      this.#products = JSON.parse(data);
+    } catch (error) {
+      console.error(`Error al buscar el archivo desde ${this.#path}: ${error}`);
     }
-    return id;
-  };
+  }
 
-  #validateField = (title, description, price, thumbnail, code, stock) => {
-    let valor;
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      valor = 0;
-      return valor;
-    } else {
-      valor = 1;
-      return valor;
+  saveProducts() {
+    try {
+      const data = JSON.stringify(this.#products, null, "\t");
+      fs.writeFileSync(this.#path, data);
+    } catch (error) {
+      console;
     }
-  };
-
-  #validateCode = (code) => {
-    let repetido = this.getElementByCode(code);
-    if (repetido == undefined) {
-      return false;
-    } else {
-      return true;
-    }
-  };
+  }
 
   getProducts = () => {
-    return this.#products;
-  };
-
-  getProductsFile = () => {
-    let contenido = fs.readFileSync("./demo.json", "utf-8");
+    let contenido = fs.readFileSync(this.#path, "utf-8");
     contenido = JSON.parse(contenido);
     return contenido;
-  };
-
-  getElementById = (id) => {
-    return this.#products.find(function (item) {
-      return item.id === id;
-    });
-  };
-
-  getElementByIdFile = (id) => {
-    let contenido = fs.readFileSync("./demo.json", "utf-8");
-    contenido = JSON.parse(contenido);
-    return contenido.find(function (item) {
-      return item.id === id;
-    });
-  };
-
-  getElementByCode = (code) => {
-    return this.#products.find(function (item) {
-      return item.code === code;
-    });
-  };
-
-  updateProduct = (id, title, description, price, thumbnail, code, stock) => {
-    let contenido = fs.readFileSync("./demo.json", "utf-8");
-    contenido = JSON.parse(contenido);
-    let updatear = this.getElementById(id);
-    updatear.title = title;
-    updatear.description = description;
-    updatear.price = price;
-    updatear.thumbnail = thumbnail;
-    updatear.code = code;
-    updatear.stock = stock;
-    contenido.push(updatear);
-    fs.writeFileSync("./demo.json", JSON.stringify(updatear, null, "\t"));
-  };
-
-  delteProduct = (id) => {
-    let contenido = fs.readFileSync("./demo.json", "utf-8");
-    contenido = JSON.parse(contenido);
-    let nuevo = contenido.filter((item) => item.id !== id);
-    fs.writeFileSync("./demo.json", JSON.stringify(nuevo, null, "\t"));
   };
 
   addProduct = (title, description, price, thumbnail, code, stock) => {
@@ -127,62 +70,122 @@ class ProductManager {
         //console.log(nuevoProducto);
         this.#products.push(nuevoProducto);
         fs.writeFileSync(
-          filename + "demo.json",
+          this.#path,
           JSON.stringify(this.#products, null, "\t")
         );
       }
     }
   };
+
+  #validateField = (title, description, price, thumbnail, code, stock) => {
+    let valor;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      valor = 0;
+      return valor;
+    } else {
+      valor = 1;
+      return valor;
+    }
+  };
+
+  #generateId = () => {
+    let id;
+    if (this.#products.length === 0) {
+      id = 1;
+    } else {
+      id = this.#products[this.#products.length - 1].id + 1;
+    }
+    return id;
+  };
+
+  #validateCode = (code) => {
+    let repetido = this.getElementByCode(code);
+    if (repetido == undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  getElementByCode = (code) => {
+    return this.#products.find(function (item) {
+      return item.code === code;
+    });
+  };
+
+  getProductById = (id) => {
+    let contenido = fs.readFileSync(this.#path, "utf-8");
+    contenido = JSON.parse(contenido);
+    return contenido.find(function (item) {
+      return item.id === id;
+    });
+  };
+
+  saveProducts() {
+    try {
+      const data = JSON.stringify(this.#products, null, "\t");
+      fs.writeFileSync(this.#path, data);
+    } catch (error) {
+      console.error(`Error al guardar el archivo en ${this.#path}: ${error}`);
+    }
+  }
+
+  updateProduct(id, title, description, price, thumbnail, code, stock) {
+    let datos = this.#products;
+    let updatear = datos.find((element) => element.id === id);
+    updatear.title = title;
+    updatear.description = description;
+    updatear.price = price;
+    updatear.thumbnail = thumbnail;
+    updatear.code = code;
+    updatear.stock = stock;
+    this.saveProducts();
+  }
+
+  getElementById = (id) => {
+    return this.#products.find(function (item) {
+      return item.id === id;
+    });
+  };
+
+  deleteProduct = (id) => {
+    let datos = this.#products;
+    datos.splice(id - 1, 1);
+    this.saveProducts();
+  };
 }
 
-const filename = "./";
+//Valores de prueba como el ejemplo
+//defino el path en la misma ruta del archivo para hacer mas facil el resto del ejercicio
+const filename = "./demo.json";
 const productManager = new ProductManager(filename);
+//Traemos los productos del archivo original
+console.log("Los productos que ya tenia el archivo son: ");
+console.log(productManager.getProducts());
 productManager.addProduct(
-  "Producto1",
-  "Producto para hacer algo",
-  1500,
-  "sdasd",
-  "A123HJ",
-  100
+  "producto de prueba",
+  "Este es un producto de prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
 );
-productManager.addProduct(
-  "Producto2",
-  "Producto 2",
-  2500,
-  "iiii",
-  "A123555",
-  200
-);
-productManager.addProduct(
-  "Producto3",
-  "Producto 3",
-  5000,
-  "fffwew",
-  "ZZ55U",
-  200
-);
-productManager.addProduct(
-  "Producto4",
-  "Producto ",
-  7000,
-  "rrrrr",
-  "L34Y78",
-  600
-);
-productManager.delteProduct(2);
-/*
+console.log("La lista de productos con el nuevo ingresado es: ");
+console.log(productManager.getProducts());
+console.log("El producto con id buscado es: ");
+console.log(productManager.getProductById(4));
 productManager.updateProduct(
-  3,
-  "ProductoZ",
-  "Producto modificado",
-  4500,
-  "prueba modificar",
-  "NNN00O",
-  100
-);*/
-//console.log("la lista de elementos es:");
-//console.log(productManager.getProducts());
-console.log("la lista de elementos es:");
-console.log(productManager.getProductsFile());
-console.log("el elemento con id 4 es: ");
-console.log(productManager.getElementByIdFile(4));
+  4,
+  "producto de prueba modificado",
+  "modifico descripcion de prueba",
+  400,
+  "sin foto",
+  "zxc987",
+  125
+);
+console.log("La lista de productos con cambio es: ");
+console.log(productManager.getProducts());
+console.log("Eliminamos el producto con id indicado: ");
+productManager.deleteProduct(4);
+console.log("La lista de productos con cambio es: ");
+console.log(productManager.getProducts());
