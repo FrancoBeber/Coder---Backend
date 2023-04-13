0c201ceb979009cc830d738eeffe715e7f26969d
Franco Beber <fbeber73@gmail.com>
2023-04-13 18:40:20 -0300

    api de ruta raiz lista

--- app.js
+++ app.js
@@ -5,11 +5,84 @@ const filename = "./demo.json";
 const productManager = new ProductManager(filename);
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send(
-    "Bienvenido... para consultar productos ingrese a la ruta /products"
-  );
+  const result = +req.query.limit;
+  if (!result) {
+    let salida = productManager.getProducts();
+    res.send({ resultado: salida });
+  } else {
+    let nuevo;
+    let arr;
+    let salida;
+    nuevo = productManager.getProducts();
+    arr = Array.from(nuevo);
+    salida = arr.slice(0, result);
+    res.send({ resultado: salida });
+  }
 });
+
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  let datos = productManager.getProductById(id);
+  if (!datos) {
+    res.send("Producto no encontrado");
+  } else {
+    res.send({ resultado: datos });
+  }
+});
+
+app.post("/", (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = +req.body.price;
+  const thumbnail = req.body.thumbnail;
+  const code = req.body.code;
+  const stock = +req.body.stock;
+  productManager.addProduct(title, description, price, thumbnail, code, stock);
+  const products = productManager.getProducts();
+  res.status(201).send({ mensaje: "Producto creeado", products });
+});
+
+app.put("/:id", (req, res) => {
+  const id = +req.params.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = +req.body.price;
+  const thumbnail = req.body.thumbnail;
+  const code = req.body.code;
+  const stock = +req.body.stock;
+  const product = productManager.getProductById(id);
+  if (!product) {
+    return res.status(404).send({ message: "Producto no encontrado" });
+  } else {
+    productManager.updateProduct(
+      id,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock
+    );
+    const products = productManager.getProducts();
+    res.status(202).send({ mensage: "Producto actualizado", products });
+  }
+});
+
+app.delete("/:id", (req, res) => {
+  const id = +req.params.id;
+  const product = productManager.getProductById(id);
+  if (!product) {
+    return res.status(404).send({ message: "Producto no encontrado" });
+  } else {
+    productManager.deleteProduct(id);
+    const products = productManager.getProducts();
+    res.status(200).send({ mensage: "Producto eliminado", products });
+  }
+});
+/*
 
 app.get("/products", (req, res) => {
   const result = +req.query.limit;
@@ -37,5 +110,5 @@ app.get("/products/:id", (req, res) => {
     res.send({ resultado: datos });
   }
 });
-
-app.listen(8080, () => console.log("Server Up"));
+*/
+app.listen(8080, () => console.log("Server listening on port: 8080"));
